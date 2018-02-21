1b0705c9ec114617b6151e20fc35038534aad0cf
Brian <brianjtherrien@gmail.com>
2018-02-21 16:30:31 -0500

    done

---
+++ jsCalc/pair.js
@@ -0,0 +1,26 @@
+var x = 10
+var y = 20
+
+
+function driver() {
+
+    runSum()
+    runDiff()
+    runPro()
+    runQuo()
+    
+}
+function runSum(){
+console.log("x = 10  y = 20 and sum is", x + y);
+}
+
+function runDiff(){
+console.log("x = 10 y = 20 and difference is", x - y);}
+
+function runPro(){
+console.log("x = 10 y = 20 and product is", x * y);
+}
+
+function runQuo(){
+console.log("x = 10 y = 20 and quotient is", x / y);
+}
