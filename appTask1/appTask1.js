946145f911dcaef9cedea24384d03c0130228798
Brian <brianjtherrien@gmail.com>
2018-05-01 22:34:27 -0400

    AppTask1
    Done

--- appTask1/appTask1.js
+++ appTask1/appTask1.js
@@ -1,55 +1,20 @@
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+  var li = document.createElement("li"); //Creates variable sets it equal to li element
+  var input = document.getElementById("personstask").value; //Creates variable and sets it equal to the personstask id
+  var text = document.createTextNode(input);  //create varialbe and set the text to the input
+  li.appendChild(text); // Add to the list
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+  if (input == '')
+    {
+      alert("Sorry, but I can't add nothing to the Task list.");
     }
-  }
+  else
+    {
+      document.getElementById("tasks").appendChild(li);
+    }
+//If there is no text in the text area "error" with and alert telling the user to input something
+
+
+  document.getElementById("personstask").value = ""; //Get what the user entered and set it equal to nothing to clear area
+  li.appendChild(span);
 }
